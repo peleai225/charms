8eb40c4f586d40b4d073d4ccc030b23d6d868fd6
peleai225 <peleai.ci@gmail.com>
2026-07-23 23:24:26 +0000

    feat(ui): Dropdown + DropdownItem (Headless UI)

---
+++ resources/js/Components/UI/Dropdown.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Menu, MenuButton, MenuItems } from '@headlessui/vue'
+</script>
+
+<template>
+    <Menu as="div" class="relative inline-block text-left">
+        <MenuButton as="template">
+            <slot name="trigger" />
+        </MenuButton>
+
+        <Transition
+            enter-active-class="transition ease-out duration-100"
+            enter-from-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75"
+            leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+        >
+            <MenuItems class="absolute right-0 z-10 mt-1 w-48 origin-top-right rounded-lg bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div class="py-1">
+                    <slot />
+                </div>
+            </MenuItems>
+        </Transition>
+    </Menu>
+</template>
---
+++ resources/js/Components/UI/DropdownItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { MenuItem } from '@headlessui/vue'
+
+const props = defineProps({
+    href:   { type: String,  default: null },
+    danger: { type: Boolean, default: false },
+})
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+    <MenuItem v-slot="{ active }">
+        <component
+            :is="href ? 'a' : 'button'"
+            :href="href"
+            :class="[
+                'flex w-full items-center px-4 py-2 text-sm transition-colors',
+                danger
+                    ? (active ? 'bg-red-50 text-red-700' : 'text-red-600')
+                    : (active ? 'bg-gray-50 text-gray-900' : 'text-gray-700'),
+            ]"
+            @click="emit('click', $event)"
+        >
+            <slot />
+        </component>
+    </MenuItem>
+</template>
